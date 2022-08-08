d9f28f64a52c3aec1adf80e02f93c0d9ea271050
Prince-Tripathi <princetripathi01@live.com>
2022-08-08 13:30:43 +0530

    Session of 08.08.22

---
+++ src/logger/logger.js
@@ -0,0 +1,7 @@
+const wel = "WelText"
+
+let anew = function () {
+    console.log("Welcome to my application. I am Prince Tripathi & part of FunctionUp Plutonium cohort.")
+}
+module.exports.name = wel
+module.exports.printName = anew
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const abc = require('../introduction/intro')
+const bca = require('../logger/logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
@@ -7,7 +8,11 @@ router.get('/test-me', function (req, res) {
     abc.printName()
     res.send('My second ever api!')
 });
-
+router.get('/test-welcome', function(req, res){
+    console.log(bca)
+    res.send('This is the second routes implementation')
+    
+});
 
 router.get('/test-you', function(req, res){
     res.send('This is the second routes implementation')
